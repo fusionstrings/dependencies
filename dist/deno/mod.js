79747a4a7b62b79c739dce68b36d92fc3d90a8c0
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-29 11:13:19 +0100

    update dependencies (#2)

--- dist/deno/mod.js
+++ dist/deno/mod.js
@@ -1,1 +1,1 @@
-export { G as Generator, c as clearCache, f as fetch, g as getPackageConfig, l as lookup } from './generator-24f509a3.js';
+export { G as Generator, c as clearCache, f as fetch, g as getPackageConfig, l as lookup } from './generator-b83dcb03.js';
--- dist/deno/url-1d370e23.js
+++
@@ -1,1 +0,0 @@
-export { a5 as URL, a4 as default, a7 as fileURLToPath, a6 as format, a8 as pathToFileURL } from './generator-24f509a3.js';
